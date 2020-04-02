bd0cc86b66c785f09927983266ae58897dab5480
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2020-04-01 23:20:31 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:giphyflutter/ui/Gif_page.dart';
+
+import 'package:giphyflutter/ui/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+
